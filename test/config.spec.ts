3cf4c9d7a3781a654f7ae49e5f55eb9a41d4c6e1
geylnu <yanglei1997630@gmail.com>
2020-05-29 17:42:35 +0800

    :white_check_mark:test: update error test

--- test/config.spec.ts
+++ test/config.spec.ts
@@ -4,7 +4,7 @@ describe("config test",()=>{
     it("can cache config after has read", async ()=>{
         const currentConfig = await config.readAndCache()
         const nextConfig = await config.readAndCache()
-        expect(currentConfig).toBe(nextConfig)
+        expect(currentConfig).toEqual(nextConfig)
     })
 })
 
